6539ed412356e54f3ad4c17fa044d56283ff3ab9
DrakshaPatle <draksha@appsdeployer.com>
2023-03-16 17:52:10 +0530

    google api

--- src/App.js
+++ src/App.js
@@ -33,3 +33,128 @@ const App = () => {
 
 export default App;
 
+
+// import 'bootstrap/dist/css/bootstrap.min.css'
+
+// import React, { useState } from 'react';
+// import { Navbar, Nav, Container, Row, Col, Form, Button } from 'react-bootstrap';
+// import { GoogleMap, LoadScript, DirectionsService, DirectionsRenderer } from '@react-google-maps/api';
+
+// const mapContainerStyle = {
+//   height: "100vh",
+//   width: "100%",
+// };
+
+// const center = {
+//   lat: 41.3851,
+//   lng: 2.1734
+// };
+
+// const options = {
+//   disableDefaultUI: true,
+// };
+
+// function App() {
+//   const [origin, setOrigin] = useState("");
+//   const [destination, setDestination] = useState("");
+//   const [response, setResponse] = useState(null);
+
+//   const directionsCallback = (res) => {
+//     if (res !== null) {
+//       setResponse(res);
+//     }
+//   }
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     if (origin !== "" && destination !== "") {
+//       setResponse(null);
+//     }
+//   }
+
+//   return (
+//     <div>
+
+
+
+//       <Navbar bg="dark" variant="dark" fixed="top">
+//         <Container>
+//           <Navbar.Brand href="#home">My App</Navbar.Brand>
+//           <Nav className="me-auto">
+//             <Nav.Link href="#home">Home</Nav.Link>
+//             <Nav.Link href="#features">Features</Nav.Link>
+//             <Nav.Link href="#pricing">Pricing</Nav.Link>
+//           </Nav>
+//         </Container>
+//       </Navbar>
+
+
+
+
+//       <Container fluid>
+
+//         <Row>
+
+
+//           <Col md={3} className="bg-light sidebar">
+
+//             {/* <Form onSubmit={handleSubmit}>
+
+
+//               <Form.Group>
+//                 <Form.Label>Origin</Form.Label>
+//                 <Form.Control type="text" placeholder="Enter origin" value={origin} onChange={(e) => setOrigin(e.target.value)} />
+//               </Form.Group>
+
+
+//               <Form.Group>
+//                 <Form.Label>Destination</Form.Label>
+//                 <Form.Control type="text" placeholder="Enter destination" value={destination} onChange={(e) => setDestination(e.target.value)} />
+//               </Form.Group>
+//               <Button variant="primary" type="submit">Submit</Button>
+//             </Form> */}
+
+
+
+            
+//           </Col>
+
+
+
+//           <Col md={9} className="map-container">
+//             <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+//               <GoogleMap
+//                 mapContainerStyle={mapContainerStyle}
+//                 center={center}
+//                 zoom={10}
+//                 options={options}
+//               >
+//                 {response !== null && (
+//                   <DirectionsRenderer
+//                     directions={response}
+//                   />
+//                 )}
+
+//                 {origin !== "" && destination !== "" && (
+//                   <DirectionsService
+//                     options={{
+//                       destination: destination,
+//                       origin: origin,
+//                       travelMode: 'DRIVING'
+//                     }}
+//                     callback={directionsCallback}
+//                   />
+//                 )}
+//               </GoogleMap>
+//             </LoadScript>
+//           </Col>
+
+
+
+//         </Row>
+//       </Container>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+
+
+
+
+import React, { useState } from 'react';
+// import { Container, Row, Form, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { GoogleMap, LoadScript, DirectionsService, DirectionsRenderer } from '@react-google-maps/api';
+
+const mapContainerStyle = {
+    height: "100%",
+    width: "100%",
+
+};
+
+const center = {
+    lat: 41.3851,
+    lng: 2.1734
+};
+
+const options = {
+    disableDefaultUI: true,
+};
+
 
 const Dashboard = () => {
+
+    const [origin, setOrigin] = useState("");
+    const [destination, setDestination] = useState("");
+    const [response, setResponse] = useState(null);
+
+    const directionsCallback = (res) => {
+        if (res !== null) {
+            setResponse(res);
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (origin !== "" && destination !== "") {
+            setResponse(null);
+        }
+    }
     return (
-        <div>
-            <h1>dashboard page</h1>
+
+
+
+        <div className="map-container">
+            <iframe src="https://maps.google.com/maps?q=Tangesir%20Dates%20Products&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" width="1300" height="1300"  allowfullscreen></iframe>
+
         </div>
     );
 };
